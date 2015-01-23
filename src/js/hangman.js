69a8998807b26af4ea36d51963aac39593efdce7
Tim Hooker <timhooker@me.com>
2015-01-22 22:40:58 -0500

    removed all words with less than three words and fixed a few logical errors

--- src/js/hangman.js
+++ src/js/hangman.js
@@ -1,7 +1,11 @@
 // Setting up my Variables
 var userGuess,
     wordIndex,
+    totalIndex = 0,
+    manyWords,
+    hangmanWords,
     dashedWord,
+    displayWord,
     word,
     finalDisplay,
     correctGuess = 'n',
@@ -16,7 +20,7 @@ var inputBox = document.querySelector('.user-input');
 
 // create an array full of words for us to use
 
-var hangmanWords = [
+manyWords = [
     "the","of","and","a","to","in","is","you","that","it","he",
     "was","for","on","are","as","with","his","they","I","at","be",
     "this","have","from","or","one","had","by","word","but","not",
@@ -30,22 +34,30 @@ var hangmanWords = [
     "come","made","may","part"
     ];
 
+function wordsWeCanUse(palabra){
+  return palabra.length >= 3;
+}
+
+// Getting Rid of words with less than 3 letters
+hangmanWords = manyWords.filter(wordsWeCanUse);
+// find total number of words we have to choose from
+totalIndex = hangmanWords.length;
+
 
 //  Set the word to be guessed
   //   generate a random result
-
-wordIndex = Math.floor(Math.random() * 100) + 1;
+wordIndex = Math.floor(Math.random() * totalIndex);
   // assign word to a variable
 word = hangmanWords[ wordIndex ];
   // split to an array
-var displayWord = word.split('');
+displayWord = word.split('');
   // generate a Dashed Display for the User
-var dashedWord = displayWord.map(function(ch){
+dashedWord = displayWord.map(function(ch){
   return '_';
-})
+});
   // generate an initial Count for the user
 deadlyCounter();
-console.log(word);
+
 showResults();
 
 //  Capture the Input
@@ -58,7 +70,6 @@ function setGuess(guessChar) {
 // Test the Result and give results back to the user
 
 function checkGuess() {
-  console.log('checkGuess');
   correctGuess = "n";
   displayWord.forEach(function(letter, i){
     if (userGuess === letter) {

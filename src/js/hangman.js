1529203be93502a7940a43b9cec50cb6bef72336
Tim Hooker <timhooker@me.com>
2015-01-24 17:27:31 -0500

    added hangman animation and associated JavaScript to reveal the illustrations

--- src/js/hangman.js
+++ src/js/hangman.js
@@ -8,7 +8,7 @@ var userGuess,
     displayWord,
     word,
     finalDisplay,
-    correctGuess = 'n',
+    correctGuess = false,
     deadCount = 9;
 
 // Transfer DOM elements into variables so we can fill them up
@@ -17,6 +17,40 @@ var userGuess,
 var guessResult = document.querySelector('.guess-result');
 var dashedWordElement = document.querySelector('.dashed-word');
 var inputBox = document.querySelector('.user-input');
+
+// Getting all Pieces of our hangman animation for use later
+// The numbers are signifying the order they will appear
+
+
+// #1 Bottom Board
+var bottomBoard = document.querySelector('.bottom-board');
+
+// #2 Tall Board
+var tallBoard = document.querySelector('.tall-board');
+
+// #3 Top Board
+var topBoard = document.querySelector('.top-board');
+
+// #4 Hangman Body
+var hangmanBody = document.querySelector('.hangman-body');
+
+// #5 Hangman Head
+var hangmanHead = document.querySelector('.hangman-head');
+var hangmanFace = document.querySelector('.hangman-face');
+
+// #6 Hangman Arms
+var hangmanArms = document.getElementsByClassName('hangman-arms');
+
+// #7 Hangman Legs
+var hangmanLegs = document.getElementsByClassName('hangman-legs')
+
+// Items to Shake
+var shake = document.getElementsByClassName('shake');
+
+// #8 Rope
+var rope = document.querySelector('.rope');
+var neckRope = document.querySelector('.neck-rope');
+
 
 // create an array full of words for us to use
 
@@ -35,11 +69,13 @@ manyWords = [
     ];
 
 function wordsWeCanUse(palabra){
-  return palabra.length >= 3;
+  return palabra.length >= 4;
 }
 
 // Getting Rid of words with less than 3 letters
 hangmanWords = manyWords.filter(wordsWeCanUse);
+// hangmanWords = manyWords.filter(elem => elem.length >= 3);
+
 // find total number of words we have to choose from
 totalIndex = hangmanWords.length;
 
@@ -64,17 +100,21 @@ showResults();
   //   Assign it to a Variable
 
 function setGuess(guessChar) {
-  userGuess = guessChar.toLowerCase();
+  if (guessChar != '') {
+    alert('you need to enter a letter');
+  } else {
+    userGuess = guessChar.toLowerCase();
+  }
 }
 
 // Test the Result and give results back to the user
 
 function checkGuess() {
-  correctGuess = "n";
+  correctGuess = false;
   displayWord.forEach(function(letter, i){
     if (userGuess === letter) {
       dashedWord[i] = letter;
-      correctGuess = "y";
+      correctGuess = true;
     }
   });
   showResults();
@@ -88,13 +128,53 @@ function checkGuess() {
 function deadlyCounter() {
   if (dashedWord.join('')===word) {
     guessResult.textContent = 'You Don\'t DIE';
-  } else if (correctGuess==='y') {
+  } else if (correctGuess===true) {
     return;
   } else if (deadCount > 1) {
     deadCount--;
+    switch (deadCount) {
+      case 7:
+        bottomBoard.style.display = 'block';
+        break;
+      case 6:
+        tallBoard.style.display = 'block';
+        break;
+      case 5:
+        topBoard.style.display = 'block';
+        break;
+      case 4:
+        hangmanBody.style.display = 'block';
+        break;
+      case 3:
+        hangmanHead.style.display = 'block';
+        break;
+      case 2:
+        for (i=0; i < hangmanArms.length; ++i) {
+          hangmanArms[i].style.display = 'block';
+        }
+        // hangmanArms.forEach(function(elem){
+        //   elem.style.display = 'block';
+        // });
+        break;
+      case 1:
+        for (i=0; i < hangmanLegs.length; ++i) {
+          hangmanLegs[i].style.display = 'block';
+        }
+        for (i=0; i < shake.length; ++i) {
+          shake[i].classList.add('shake-it');
+        }
+        break;
+    }
     guessResult.textContent = 'You have ' +  deadCount + ' guesses left';
   } else {
     guessResult.textContent = 'You lose';
+    rope.style.display = 'block';
+    neckRope.style.display = 'block';
+    for (i=0; i < shake.length; ++i) {
+      shake[i].classList.remove('shake-it');
+    }
+    hangmanHead.classList.add('hangman-dead');
+    hangmanFace.style.display = 'none';
   }
 }
 
